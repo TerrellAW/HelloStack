4d1be80ffae6e3364a29a59d4363fa4fd6321d09
TerrellAW <terraw1999@proton.me>
2025-12-11 11:36:41 -0700

    Add: Push and pop implementation

--- stack.s
+++ stack.s
@@ -4,6 +4,8 @@
 
 // Entry point
 _start:
-	mov		x0, #0 		// Set status to 0
-	mov		w8, #93		// Call exit
-	svc		#0			// Invoke syscall
+	mov		x1, #42 				// Set register 1 to 42
+	stp		x1, xzr, [sp, #-16]!	// Push register 1 and an unused register to stack and allocate 16 bytes
+	ldp		x0,  x1, [sp], #16		// Pop value from stack into register 0, set register 1 back to null
+	mov		w8, #93					// Call exit, will use value in register 0 as exit code
+	svc		#0						// Invoke syscall
